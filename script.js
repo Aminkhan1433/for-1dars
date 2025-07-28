9d974fcd030805d9b0b5a855665fb7bd1c3af296
Aminkhan1433 <aminkhansayfullayev@gmail.com>
2025-07-28 15:04:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let data = [1,2,3,4,5,6,7,8,9]
+
+
+console.log(data);
+
+
+
+let toq = []
+
+let juft = []
+
+
+for (let i = 0; i < data.length; i++) {
+    if(data[i] % 2 == 0) {
+         juft.push(data[i])
+    }else{
+       toq.push(data[i])
+    }
+}
+
+
+
+console.log(toq);
+console.log(juft);
+
